d39089e72c843b32f41f75851f65a0f68ae9d254
Alessandro Pezzè <igougi.ui@gmail.com>
2016-08-07 22:00:00 +0200

    Fixes #15

--- src/default.js
+++ src/default.js
@@ -3,6 +3,8 @@ const values = {};
 values.protocol = 'http';
 values.hostName = '://pokeapi.co';
 values.versionPath = '/api/v2/';
+values.offset = 0;
+values.limit = 100000;
 
 values.setProtocol = (newProtocol) => {
     values.protocol = newProtocol;
@@ -12,6 +14,12 @@ values.setHostName = (newHostName) => {
 }
 values.setVersionPath = (newVersionPath) => {
     values.versionPath = newVersionPath;
+}
+values.setOffset = (newOffset) => {
+    values.offset = newOffset;
+}
+values.setLimit = (newLimit) => {
+    values.limit = newLimit;
 }
 
 export { values };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import async from 'async';
 
 import endpoints from './endpoints.json';
+import rootEndpoints from './rootEndpoints.json';
 import { values } from './default.js';
 import { getJSON } from './getter.js';
 
@@ -26,20 +27,20 @@ const Pokedex = (() => {
 
                 // if the user has submitted a Name or an Id, return the Json promise
                 if (typeof input === 'number' || typeof input === 'string') {
-                    return getJSON(values.protocol + values.hostName + values.versionPath + endpoint[1] + '/' + input + '/', cb); 
+                    return getJSON(`${values.protocol}${values.hostName}${values.versionPath}${endpoint[1]}/${input}/`, cb); 
                 }
 
                 // if the user has submitted an Array
                 // return a new promise which will resolve when all getJSON calls are ended
                 else if (typeof input === 'object') {
-                    let toReturn = []
+                    let toReturn = [];
                     return new Promise((resolve, reject) => {
 
                         // fetch data asynchronously to be faster
                         async.forEachOf(input, (name) => {
 
                             //get current input data and then try to resolve
-                            getJSON(values.protocol + values.hostName + values.versionPath + endpoint[1] + '/' + name + '/', (response) => {
+                            getJSON(`${values.protocol}${values.hostName}${values.versionPath}${endpoint[1]}/${name}/`, (response) => {
                                 toReturn.push(response);
                                 if(toReturn.length === input.length){
                                     if (cb) {
@@ -48,12 +49,27 @@ const Pokedex = (() => {
                                     resolve(toReturn);
                                 }
                             });
-                        })
+                        });
                     });
                 }
             }
         }
     });
+
+    rootEndpoints.forEach((rootEndpoint) => {
+        Pokedex.prototype[rootEndpoint[0]] = (config, cb) => {
+            if (config) {
+                if (config.offset) {
+                    values.setOffset(config.offset);
+                }
+                if (config.limit) {
+                    values.setLimit(config.limit);
+                }
+            }
+            return getJSON(`${values.protocol}${values.hostName}${values.versionPath}${rootEndpoint[1]}?limit=${values.limit}&offset=${values.offset}`, cb);
+        }
+    });
+
     return Pokedex;
 })();
 
