c700d5e1bd357692fc6fc1bcc8704d2134b30672
Naramsim <igougi.ui@gmail.com>
2019-06-11 17:59:38 +0200

    refactor: use async/await

--- src/getter.js
+++ src/getter.js
@@ -3,66 +3,60 @@ const cache = require('memory-cache')
 
 const { values } = require('./default.js')
 
-exports.getJSON = (url, cb) => {
+// retrive data from the web
+let options = {
+    baseURL: `${values.protocol}${values.hostName}/`,
+    timeout: values.timeout
+}
+
+exports.getJSON = async (url, cb) => {
     
     // retrive possible content from volatile memory
     const cachedResult = cache.get(url);
     if (cachedResult !== null) {
-        return new Promise((resolve, reject) => {
-            if (cb) {
-                // call callback without errors
-                cb(cachedResult, false);
-            }
-            resolve(cachedResult);
-        });
-    } else {
-        // retrive data from the web
-        let options = {
-            baseURL: `${values.protocol}${values.hostName}/`,
-            timeout: values.timeout
+        if (cb) {
+            // call callback without errors
+            cb(cachedResult, false);
         }
-        return axios.get(url, options)
-            .catch(error => {
+        return cachedResult
+    } else {
+        try{
+            response = await axios.get(url, options)       
+            // if there is an error
+            if (response.statusCode !== undefined && response.statusCode !== 200) {
                 if (!cb) {
                     // throw if a Promise
-                    throw error;
+                    throw response;
                 } else {
                     // call the callback with error as second parameter
-                    cb('error', error);
+                    cb('error', response);
                 }
-            })
-            .then(response => {
-                if (response) {
-
-                    // if there was an error
-                    if (response.statusCode !== undefined && response.statusCode !== 200) {
-                        if (!cb) {
-                            // throw if a Promise
-                            throw response;
-                        } else {
-                            // call the callback with error as second parameter
-                            cb('error', response);
-                        }
-                    } else {
-                        // if everything was good
-                        // cache the object in volatile memory
-                        // only if cacheLimit > 0
-                        response = response.data
-
-                        if (values.cacheLimit > 0) {
-                            cache.put(url, response, values.cacheLimit);
-                        }
-
-                        // if a callback is present
-                        if (cb) {
-                            // call it, without errors
-                            cb(response, false);
-                        } else {
-                            // return the Promise
-                            return response;
-                        }
-                    }
+            } else {
+                // if everything was good
+                // cache the object in volatile memory
+                // only if cacheLimit > 0
+                response = response.data
+                
+                if (values.cacheLimit > 0) {
+                    cache.put(url, response, values.cacheLimit);
                 }
-            }); 
+                
+                // if a callback is present
+                if (cb) {
+                    // call it, without errors
+                    cb(response, false);
+                } else {
+                    return response;
+                }
+            }
+        } catch (error) {
+            if (!cb) {
+                // throw if a Promise
+                throw error;
+            } else {
+                // call the callback with error as second parameter
+                cb('error', error);
+            }
+        }
     }
 }
--- src/index.js
+++ src/index.js
@@ -12,54 +12,60 @@ class Pokedex {
         
         // add to Pokedex.prototype all our endpoint functions
         endpoints.forEach(endpoint => {
-            this[endpoint[0]] = (input, cb) => {
-                const mapper = async name => {
-                    const queryRes = await getJSON(`${values.protocol}${values.hostName}${values.versionPath}${endpoint[1]}/${name}/`)
-                    return queryRes;
-                };
-
-                if (input) {
-
-                    // if the user has submitted a Name or an Id, return the Json promise
-                    if (typeof input === 'number' || typeof input === 'string') {
-                        return getJSON(`${values.protocol}${values.hostName}${values.versionPath}${endpoint[1]}/${input}/`, cb); 
-                    }
-
-                    // if the user has submitted an Array
-                    // return a new promise which will resolve when all getJSON calls are ended
-                    else if (typeof input === 'object') {
-                        return new Promise((resolve, reject) => {
-
-                            
-
+            this[endpoint[0]] = async (input, cb) => {
+                try {
+                    const mapper = async name => {
+                        const queryRes = await getJSON(`${values.protocol}${values.hostName}${values.versionPath}${endpoint[1]}/${name}/`)
+                        return queryRes;
+                    };
+    
+                    if (input) {
+    
+                        // if the user has submitted a Name or an Id, return the Json promise
+                        if (typeof input === 'number' || typeof input === 'string') {
+                            return getJSON(`${values.protocol}${values.hostName}${values.versionPath}${endpoint[1]}/${input}/`, cb); 
+                        }
+    
+                        // if the user has submitted an Array
+                        // return a new promise which will resolve when all getJSON calls are ended
+                        else if (typeof input === 'object') {
                             // fetch data asynchronously to be faster
-                            pMap(input, mapper, {concurrency: 4}).then(toReturn => {
-                                if (cb) {
-                                    cb(toReturn);
-                                }
-                                resolve(toReturn);
-                            })
-                        });
+                            const toReturn = await pMap(input, mapper, {concurrency: 4})
+                            if (cb) {
+                                cb(toReturn);
+                            }
+                            return toReturn;
+                        }
                     }
+                } catch (error) {
+                    throw error
                 }
             }
         });
 
         rootEndpoints.forEach(rootEndpoint => {
-            this[rootEndpoint[0]] = (config, cb) => {
-                configurator.setRootEndpointConfiguration(config);
-                return getJSON(`${values.protocol}${values.hostName}${values.versionPath}${rootEndpoint[1]}?limit=${values.limit}&offset=${values.offset}`, cb);
+            this[rootEndpoint[0]] = async (config, cb) => {
+                try {
+                    configurator.setRootEndpointConfiguration(config);
+                    return getJSON(`${values.protocol}${values.hostName}${values.versionPath}${rootEndpoint[1]}?limit=${values.limit}&offset=${values.offset}`, cb)
+                } catch (error) {
+                    throw error
+                }
             }
         });
     }
 
-    resource(path) {
-        if (typeof path === 'string') {
-            return getJSON(path)
-        } else if (typeof path === 'object') {
-            return Promise.all(path.map(p => getJSON(p)));
-        } else {
-            return 'String or Array is required'
+    async resource(path) {
+        try{
+            if (typeof path === 'string') {
+                return getJSON(path)
+            } else if (typeof path === 'object') {
+                return Promise.all(path.map(p => getJSON(p)));
+            } else {
+                throw 'String or Array required'
+            }
+        } catch (error) {
+            throw error
         }
     }
 };
