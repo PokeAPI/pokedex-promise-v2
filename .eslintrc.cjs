1e6254e98e567b32c4772fccdcc0e45c3774b4d8
Helton Reis <heltonkreis@gmail.com>
2021-10-25 19:31:57 -0300

    [chore]{wip} Setup everything for full TypeScript support

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,20 @@
 module.exports = {
   env: {
-    browser: true,
     es2021: true,
     node: true,
   },
   extends: [
     'airbnb-base',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
+  plugins: [
+    '@typescript-eslint',
+  ],
   rules: {
-    'consistent-return': 'off',
-    'no-prototype-builtins': 'off',
-    'no-shadow': 'off',
     'import/extensions': 'off',
   },
 };
