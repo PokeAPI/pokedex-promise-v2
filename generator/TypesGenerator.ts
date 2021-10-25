cd256dfa18041c58acd0f761b1253bf38a97e1fa
Helton Reis <heltonkreis@gmail.com>
2021-10-25 19:38:44 -0300

    [refactor]{!!!/wip} Cleanup Types Generator

--- generator/TypesGenerator.ts
+++ generator/TypesGenerator.ts
@@ -2,39 +2,152 @@ import fs from 'fs';
 import path from 'path';
 import directoryTree from 'directory-tree';
 import { Project, Writers } from 'ts-morph';
-import { quicktype, InputData, JSONSchemaInput, FetchingJSONSchemaStore } from 'quicktype-core';
+import {
+  quicktype, InputData, JSONSchemaInput, FetchingJSONSchemaStore,
+} from 'quicktype-core';
 
-import endpoints from '../src/endpoints.js';
-import rootEndpoints from '../src/rootEndpoints.js';
-import { schemaFolder, timerLabel, typeFile } from './Utils.js';
+import {
+  apiMapFile, schemaFolder, typesLabel, typeFile,
+} from './Utils.js';
+
+console.log(schemaFolder);
+if (!fs.existsSync(schemaFolder)) {
+  console.log('Schemas not found, please clone the "PokeAPI/api-data" repository on the root of this project first and change all $refs from "/schema/v2" to "api-data/data/schema/v2")');
+  process.exit(1);
+}
 
 // Helper functions to interface names
+function clearAndUpper(text: string) {
+  return text.replace(/-/, '').toUpperCase();
+}
+
 function toPascalCase(text: string) {
   return text.replace(/(^\w|-\w)/g, clearAndUpper);
 }
 
-function clearAndUpper(text: string) {
-  return text.replace(/-/, "").toUpperCase();
+// A map for the methods of the class
+const apiMap: Record<string, string> = {};
+
+// The method that will string together the types and the PokeAPI class
+async function generateFinalFile(types: string) {
+  // Log progress...
+  console.timeLog(typesLabel, '- Types generated, starting the generation of the definition file...');
+
+  // Initialize the types file
+  const project = new Project();
+  const file = project.createSourceFile(typeFile, `/*
+* Type definitions for pokedex-promise-v2 v4.x
+* DO NOT MODIFY, THIS IS AUTO GENERATED
+* Code by: HRKings <https://github.com/HRKings/>
+* And: Christian Garza <https://github.com/C-Garza/>
+* Code inspired by: Mudkip <https://github.com/mudkipme/>
+* Execute \`npm run generate\` to regenerate
+*/`, { overwrite: true });
+
+  // Create the root module
+  const rootModule = file.addModule({
+    name: '\'pokedex-promise-v2\'',
+  });
+
+  // Create the namespace
+  const namespace = rootModule.addModule({
+    name: 'PokeAPI',
+  });
+
+  // Write the interfaces to the namespace
+  namespace.setBodyText(types);
+
+  // Remove interfaces that are wrongly generated
+  namespace.getInterface('EvolutionChainElement').remove();
+  namespace.getInterface('GenerationElement').remove();
+  namespace.getInterface('VersionGroupNamedList').remove();
+
+  // Replace the wrong definitions with the correct ones
+  namespace.setBodyText(namespace.getBodyText()
+    .replaceAll('EvolutionChainElement', 'APIResource')
+    .replaceAll('GenerationElement', 'NamedAPIResource'));
+
+  // Format the namespace to be correctly indented
+  namespace.formatText();
+
+  // Add the root endpoint interval
+  namespace.addInterface({
+    name: 'ListEndpointOptions',
+    properties: [{
+      name: 'limit',
+      type: 'number',
+      hasQuestionToken: true,
+    }, {
+      name: 'offset',
+      type: 'number',
+      hasQuestionToken: true,
+    }, {
+      name: 'cacheLimit',
+      type: 'number',
+      hasQuestionToken: true,
+    }],
+  });
+
+  // Add the options interface
+  rootModule.addInterface({
+    name: 'PokeAPIOptions',
+    properties: [{
+      name: 'protocol',
+      type: Writers.unionType('\'https\'', '\'http\''),
+      hasQuestionToken: true,
+    }, {
+      name: 'hostName',
+      type: 'string',
+      hasQuestionToken: true,
+    }, {
+      name: 'versionPath',
+      type: 'string',
+      hasQuestionToken: true,
+    }, {
+      name: 'offset',
+      type: 'number',
+      hasQuestionToken: true,
+    }, {
+      name: 'limit',
+      type: 'number',
+      hasQuestionToken: true,
+    }, {
+      name: 'timeout',
+      type: 'number',
+      hasQuestionToken: true,
+    }, {
+      name: 'cacheLimit',
+      type: 'number',
+      hasQuestionToken: true,
+    }],
+  });
+
+  // Write the file
+  await file.save();
+
+  // Write the API Map
+  fs.writeFileSync(apiMapFile, JSON.stringify(apiMap, null, 2));
+
+  // Log progress...
+  console.timeEnd(typesLabel);
+  console.log('Definition file created!');
 }
 
-// A map for the methods of the class
-let apiMap: Record<string, string> = {};
-
 // Log start
-console.time(timerLabel);
-console.timeLog(timerLabel, '- Starting to generate types...');
+console.time(typesLabel);
+console.timeLog(typesLabel, '- Starting to generate types...');
 
 // Init quicktype stuff
 const schemaInput = new JSONSchemaInput(new FetchingJSONSchemaStore());
 const inputData = new InputData();
 
 // Gets schema file and adds it to schema source, outputs file at end
-async function quicktypeMain(jsonSchema, basename) {
+async function quicktypeMain(jsonSchema: string, basename: string) {
   // Add a single schema file to the schemaInput
   await schemaInput.addSource({ name: basename, schema: jsonSchema });
 
   // If its the last schema to process
-  if (basename === "VersionGroupNamedList") {
+  if (basename === 'VersionGroupNamedList') {
     // Adds the last file to the schema
     inputData.addInput(schemaInput);
 
@@ -44,17 +157,17 @@ async function quicktypeMain(jsonSchema, basename) {
       lang: 'TypeScript',
       rendererOptions: {
         'just-types': 'true',
-      }
+      },
     });
 
-    await generateFinalFile(qt.lines.join("\n").replaceAll('export ', ''));
+    await generateFinalFile(qt.lines.join('\n').replaceAll('export ', ''));
   }
-};
+}
 
 // First pass through directory tree to make sure standalone files
 // are added to schema source first
 const tree = directoryTree(schemaFolder, { extensions: /\.json$/, normalizePath: true });
-tree.children.forEach(child => {
+tree.children.forEach((child) => {
   if (!child.children) {
     // Get the file name
     let basename = path.basename(child.path, '.json').replaceAll('_', '-');
@@ -67,7 +180,7 @@ tree.children.forEach(child => {
     }
 
     // Read all the schema file
-    const jsonSchema = fs.readFileSync(child.path, "utf8");
+    const jsonSchema = fs.readFileSync(child.path, 'utf8');
     quicktypeMain(jsonSchema, basename);
   }
 });
@@ -75,12 +188,11 @@ tree.children.forEach(child => {
 // Loops through schema directory with main logic
 directoryTree(schemaFolder, { extensions: /\.json$/, normalizePath: true }, (item) => {
   // Split the path, to get the folder names later
-  let paths = item.path.split('/').reverse();
+  const paths = item.path.split('/').reverse();
 
   // Don't add the standalone files again to the schema source
   // also, don't parse the -1 folder
   if (paths[1] !== 'v2' && !item.path.includes('move-ailment/-1')) {
-
     // The endpoint path, as in the endpoints list
     let basename: string;
     // The name of the generated interface/type
@@ -90,17 +202,15 @@ directoryTree(schemaFolder, { extensions: /\.json$/, normalizePath: true }, (ite
     if (item.path.includes('pokemon/$id/encounters')) {
       basename = 'pokemon-encounter';
       interfaceName = 'PokemonEncounter';
-    }
-
-    // If the scheme is the wanted one, pick the name of two folder above, eg.: 'pokemon/$id/index.json', picks the "pokemon"
-    else if (item.path.includes('$id')) {
+    } else if (item.path.includes('$id')) {
+      // If the scheme is the wanted one, pick the name of two folder above
+      // eg.: 'pokemon/$id/index.json', picks the "pokemon"
       basename = paths[2];
       interfaceName = toPascalCase(basename);
-    }
-
-    // Gets one folder above (used for the resource lists), eg. 'pokemon/index.js', picks the "pokemon"
-    // and adds 'List' to the interface name, eg. 'PokemonList'
-    else {
+    } else {
+      // Gets one folder above (used for the resource lists)
+      // eg. 'pokemon/index.js', picks the "pokemon"
+      // and adds 'List' to the interface name, eg. 'PokemonList'
       basename = `${paths[1]}-list`;
       interfaceName = toPascalCase(basename);
     }
@@ -109,7 +219,7 @@ directoryTree(schemaFolder, { extensions: /\.json$/, normalizePath: true }, (ite
     apiMap[basename] = interfaceName;
 
     // Read all the schema file
-    const jsonSchema = fs.readFileSync(item.path, "utf8");
+    const jsonSchema = fs.readFileSync(item.path, 'utf8');
 
     // If the interface contains named resources and is a list, rename the interface
     if (interfaceName.includes('List') && jsonSchema.includes('named_api_resource_list.json')) {
