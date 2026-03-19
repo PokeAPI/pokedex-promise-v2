a3aea5090232059886d444ab4bae9ea819797303
Helton Reis <hrkings@rhaizzing.com>
2026-03-19 00:30:37 -0300

    tool: migrate to eslint 10

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-/* eslint-disable */
+ 
 /*
 * DO NOT MODIFY, THIS IS AUTO GENERATED
 * Execute `npm run generate` to regenerate
--- src/utils/ErrorHandler.ts
+++ src/utils/ErrorHandler.ts
@@ -1,5 +1,5 @@
-/* eslint-disable no-shadow */
-/* eslint-disable no-unused-vars */
+ 
+ 
 function handleError(error: any, callback?: (result: any, error?: boolean | any) => any) {
   if (callback) {
     callback('Pokedex-promise-v2 error', error);
